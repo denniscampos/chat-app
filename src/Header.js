d1feb939270aa5cb6a05842f3b70ce4f8fd30422
Dennis Campos <dennisjr89@gmail.com>
2021-10-26 16:22:53 -0400

    revamped header with tailwind

--- src/Header.js
+++ src/Header.js
@@ -1,20 +1,71 @@
-import Auth from "./Auth";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { getAuth } from "firebase/auth";
+import {
+  getAuth,
+  signOut,
+  signInWithRedirect,
+  GoogleAuthProvider,
+} from "firebase/auth";
+// import logo from "./chat-boxy-logo.svg";
+import { ReactComponent as Logo } from "./chat-boxy-logo.svg";
 
 const Header = () => {
   const auth = getAuth();
   const [user, loading, error] = useAuthState(auth);
 
+  const login = async () => {
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth();
+    await signInWithRedirect(auth, provider);
+  };
+
+  const logout = async () => {
+    try {
+      await signOut(auth);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  if (loading) {
+    return (
+      <div>
+        <p>Logging in User...</p>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div>
+        <p>Error: {error}</p>
+      </div>
+    );
+  }
+
   return (
-    <div className="container flex flex-col justify-center items-center border-b-8 border-backgroundSecondary">
-      <h1 className="text-4xl font-bold my-6 text-textPrimary">Chat Boxy</h1>
-      {user ? (
-        <span className="text-4xl my-4 text-textSecondary">
-          Welcome:
-          {" " + user.displayName}
-        </span>
-      ) : null}
+    <div className="header flex flex-row border-b-4 border-colorThree py-10 justify-between">
+      <div className="header__left flex px-5 items-center">
+        {/* <img className="object-contain h-16 text-colorTwo" src={logo} alt="" /> */}
+        <Logo className="object-contain h-16 px-5" fill="#ff6d00" />
+        <h1 className="text-4xl text-colorOne px-5 font-bold">Chat Boxy</h1>
+      </div>
+      <div className="header__right px-5 flex items-center">
+        {user ? (
+          <button
+            onClick={logout}
+            className="rounded-md bg-bgTwo text-colorOne px-8 py-2 font-bold"
+          >
+            Sign Out
+          </button>
+        ) : (
+          <button
+            onClick={login}
+            className="rounded-md bg-bgTwo text-colorOne px-8 py-2 font-bold"
+          >
+            Sign In
+          </button>
+        )}
+      </div>
     </div>
   );
 };
--- src/Message.js
+++ src/Message.js
@@ -8,13 +8,26 @@ const Message = ({ text }) => {
 
   return (
     <div>
-      <div className="user__information flex items-center py-6">
+      <div className="user__information">
         {user.photoURL ? (
+          <img className="rounded-full w-20" src={user.photoURL} alt="Avatar" />
+        ) : null}
+        {user.displayName ? (
+          <span className="text-2xl ml-2 text-textSecondary">
+            {user.displayName}
+          </span>
+        ) : null}
+        {user.displayName ? (
+          <span className="text-2xl ml-2 text-textSecondary">
+            Today at 7:32 PM
+          </span>
+        ) : null}
+        {/* {user.photoURL ? (
           <img className="rounded-full w-20" src={user.photoURL} alt="avatar" />
         ) : null}
         {user.displayName ? (
           <p className="text-2xl ml-2 text-textSecondary">{user.displayName}</p>
-        ) : null}
+        ) : null} */}
       </div>
     </div>
   );
